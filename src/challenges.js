d0462c402721c01b994cf7dee86da07ab48aed7d
alissonRH <alisson.honorato@live.com>
2022-02-08 17:47:01 -0300

    pausa pro lanche. Ate 6 completo

--- src/challenges.js
+++ src/challenges.js
@@ -5,33 +5,59 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(myString) {
+  
+  let splits = myString.split(' '); // torna a string em array
+  return splits
 }
+
+/* console.log(splitSentence('Alisson Rech Honorato VENI sartori'));
+
+
+  let myString = 'Alisson Rech Honorato VENI sartori';
+  let splits = myString.split(' ');
+  
+  console.log(splits);  */
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(myArray) {
+  let first = myArray.shift() // remove o primeiro elemento da array
+  let last = myArray.pop() // remove o ultimo elemento da array
+  let newArray = [last, first].join(', '); // separa com espaço os elemetos do array em string 
+  //let concatArray = newArray.join(', ');
+  return newArray
 }
 
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let result = (wins * 3) + (ties * 1);
+  return result
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  
+  let max = Math.max(...array);
+  let contador = 0;
+  for (let i = 0; i < array.length; i+= 1) {
+      if (array[i] == max) {
+        contador += 1;
+    }  
+  }    
+  return contador 
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (mouse < (cat1 || ))
+
 }
 
 // Desafio 8
