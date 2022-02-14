551525dae98b33a762038f447635bc3b37fa8e66
alissonRH <alisson.honorato@live.com>
2022-02-14 12:21:57 -0300

    ...

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,31 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length < 11) {
+    return "Array com tamanho incorreto"
+  } else if (array.length == 11 ) {
+    let telNumber = array.toString();
+    return telNumber
+  }
 }
 
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-}
+   if (
+    lineA < (lineC + lineB) && lineA > (Math.abs(lineC - lineB))
+     &&
+    lineB < (lineA + lineC) && lineB > (Math.abs(lineC - lineB))
+     &&
+    lineC < (lineA + lineB) && lineC > (Math.abs(lineC - lineB)) 
+    ) 
+    { return true
+    } else {
+     return false }
+  }
+  
+ 
 
 // Desafio 13
 function hydrate(frase) {
