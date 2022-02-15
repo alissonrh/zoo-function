5f1d59217ce3e55c020251ce78e4fe69850b4650
alissonRH <alisson.honorato@live.com>
2022-02-14 21:37:48 -0300

    final

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,23 @@
 // Desafio 11
 function generatePhoneNumber(array) {
-  let counter = 0;
+  
   //console.log(array.length);
   if (array.length !== 11) {
      return 'Array com tamanho incorreto.' }  
 
   for (let i = 0; i < array.length; i += 1) {
-    if ( array[i] < 0 && array[i] > 9){ 
+    let counter = 0;
+    if ( array[i] < 0 || array[i] > 9){ 
       return "não é possível gerar um número de telefone com esses valores"  
     } 
     for (let index = 0; index < array.length; index += 1) {
-     if (array[i] == array[index])
-      { counter += 1; 
-        if (counter > 2){
-          return 'não é possível gerar um número de telefone com esses valores'
-        }  
+      
+     if (array[i] === array[index])      
+     { counter += 1; }
       }
-    }
+      if (counter > 2) {
+        return "não é possível gerar um número de telefone com esses valores"  
+      }  
   } 
   return "("+array[0]+array[1]+")"+" "+ array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10];
 }
